51fe50a15f27a50d05a50f9b24b7248e8aba571f
ravipatrayadi <ravipatrayadi@gmail.com>
2024-01-15 16:57:32 +0530

    babel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x3R3xbf1x0ZFREal5QTnZYUj0eQnxTdEFjWH9bcXZXR2BbWERxVg==');
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NAaF5cWWZCfExzWmFZfVpgdl9HYFZRTGYuP1ZhSXxXdkRiXH9YcnFQRWhUVkc=');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
